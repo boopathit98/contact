d081cb77848963d75203a357baf7aa0ff4cff21e
boopathi <boopathit98@gmail.com>
2019-06-20 09:55:06 +0530

    initial

---
+++ src/Models/ContactForm.php
@@ -0,0 +1,10 @@
+<?php
+namespace boopathi\contact\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactForm extends Model
+{
+    protected $guarded = [];
+
+    protected $table = 'contact';
+}
